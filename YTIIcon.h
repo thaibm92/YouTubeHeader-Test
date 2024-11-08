c4ada085b7c536bdc5e3275f699728ea980f2fc8
Minh Thái <68208369+thaibm92@users.noreply.github.com>
2024-11-08 22:06:09 +0700

    Update YTIIcon.h

--- YTIIcon.h
+++ YTIIcon.h
@@ -1,6 +1,8 @@
+#import <UIKit/UIKit.h>
 #import "GPBMessage.h"
 #import "YTIcon.h"
 
 @interface YTIIcon : GPBMessage
-@property (nonatomic, assign, readwrite) YTIcon iconType;
+@property (nonatomic, strong, readwrite) UIImage *image;  // Thay vì iconType, giờ dùng UIImage
+- (instancetype)initWithImageFromPath:(NSString *)path;  // Phương thức khởi tạo từ file hình ảnh
 @end
