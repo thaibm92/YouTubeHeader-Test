e718a7d7a3a22e811ed03b69cd4d098d8c258f72
Minh Thái <68208369+thaibm92@users.noreply.github.com>
2024-11-08 22:10:12 +0700

    Update YTIIcon.h

--- YTIIcon.h
+++ YTIIcon.h
@@ -6,3 +6,19 @@
 @property (nonatomic, strong, readwrite) UIImage *image;  // Thay vì iconType, giờ dùng UIImage
 - (instancetype)initWithImageFromPath:(NSString *)path;  // Phương thức khởi tạo từ file hình ảnh
 @end
+
+@implementation YTIIcon
+
+// Phương thức khởi tạo từ file hình ảnh
+- (instancetype)initWithImageFromPath:(NSString *)path {
+    self = [super init];
+    if (self) {
+        _image = [UIImage imageWithContentsOfFile:path];
+        if (!_image) {
+            NSLog(@"Không thể tải hình ảnh từ đường dẫn: %@", path);
+        }
+    }
+    return self;
+}
+
+@end
